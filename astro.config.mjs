94f48974a456fabdf9ab04eaa09d3a371058d043
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-07-13 17:15:29 -0300

    feat: add sanity stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
+import react from '@astrojs/react';
+import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: 'rjigjic0',
+      dataset: 'production',
+      apiVersion: '2025-01-28',
+      useCdn: false,
+      studioBasePath: '/studio',
+    }),
+    react(),
+  ],
+});
---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const SANITY_PROJECT_ID = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
+export const SANITY_DATASET = import.meta.env.PUBLIC_SANITY_DATASET;
