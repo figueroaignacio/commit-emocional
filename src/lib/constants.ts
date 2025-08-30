d2726bdc662a5c0071087a59c4dc376305bc9cda
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-08-30 02:17:19 -0300

    feat: remove all files to start nextjs migration

--- src/lib/constants.ts
+++
@@ -1,2 +0,0 @@
-export const SANITY_PROJECT_ID = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
-export const SANITY_DATASET = import.meta.env.PUBLIC_SANITY_DATASET;
--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateInput: string | Date): string {
-  const date = new Date(dateInput);
-  return date.toLocaleDateString('es-AR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  });
-}
