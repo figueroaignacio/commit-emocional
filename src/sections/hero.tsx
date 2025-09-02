70b46e5fa21d3759059879179cfabae40163df9e
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-09-02 19:20:10 -0300

    feat: change hero

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,21 +1,13 @@
 // Components
-import { ArrowRightIcon } from '@radix-ui/react-icons'
-import Link from 'next/link'
 
 export function Hero() {
   return (
-    <section className="container min-h-[50lvh] flex flex-col justify-center gap-y-4">
-      <h1 className="text-3xl font-bold">Bienvenido a mi espacio personal</h1>
-      <p className="text-muted-foreground">
+    <section className="container min-h-[20lvh] flex flex-col justify-center gap-y-4 my-7">
+      <h1 className="text-2xl font-bold">Bienvenido a mi espacio personal</h1>
+      <p className="text-muted-foreground font-sm">
         Qué se yo… soy un pibe que piensa demasiado y escribe cuando el caos mental aprieta.
         Bienvenido a donde nada está del todo claro, pero algo siempre sale.
       </p>
-      <Link
-        href="/articles"
-        className="bg-primary rounded-lg px-4 py-2 text-sm text-white w-fit hover:scale-[1.03] active:scale-[0.97] transition duration-75 flex items-center gap-x-2"
-      >
-        Explorar articulos <ArrowRightIcon />
-      </Link>
     </section>
   )
 }
