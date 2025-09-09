28ed9c09527fac0e10daf86bd58c6a7fa8445cc4
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-09-09 17:22:55 -0300

    feat: change hero font size

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -6,8 +6,8 @@ type HeroProps = {
 export function Hero({ description, title }: HeroProps) {
   return (
     <section className="container min-h-[20lvh] flex flex-col justify-center gap-y-4 my-7">
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <p className="text-muted-foreground font-sm">{description}</p>
+      <h1 className="text-xl font-bold">{title}</h1>
+      <p className="text-muted-foreground text-sm">{description}</p>
     </section>
   )
 }
