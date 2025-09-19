17582b9d7d1ce96922b602e304a37cf1d9a618ae
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-09-18 22:51:28 -0300

    feat: improve post card

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -15,7 +15,7 @@ export function PostCard({
   publishedDate,
 }: Pick<Post, 'slug' | 'featured' | 'title' | 'description' | 'publishedDate'>) {
   return (
-    <div>
+    <div className="border-l-8 border-primary rounded-md p-4">
       <div className="flex items-center justify-between mb-3">
         <Link href={`/posts/${slug}`}>
           <h3 className="text-lg font-semibold transition-colors">{title}</h3>
@@ -23,12 +23,14 @@ export function PostCard({
         <span className="text-xs text-muted-foreground">{resetDateLocal(publishedDate)}</span>
       </div>
       <p className="text-muted-foreground text-sm leading-relaxed mb-4">{description}</p>
-      <Link
-        href={`/posts/${slug}`}
-        className="inline-flex items-center gap-x-2 text-sm text-muted-foreground hover:text-primary transition-colors font-medium mb-5"
-      >
-        Leer más <ArrowRightIcon size={14} />
-      </Link>
+      <div className="flex w-full justify-end">
+        <Link
+          href={`/posts/${slug}`}
+          className="inline-flex items-center gap-x-2 text-sm text-muted-foreground hover:text-primary transition-colors font-medium mb-5"
+        >
+          Leer más <ArrowRightIcon size={14} />
+        </Link>
+      </div>
     </div>
   )
 }
