281d12cc2239f7eddd1f50d28bdf824f622dc84d
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-09-30 08:26:44 -0300

    fix: services

--- src/app/(www)/page.tsx
+++ src/app/(www)/page.tsx
@@ -2,13 +2,16 @@
 import { FeaturedPosts } from '@/sections/featured-posts';
 import { Hero } from '@/sections/hero';
 
+const info = {
+  title: 'Bienvenido a mi espacio personal',
+  description:
+    'Qué se yo… soy un pibe que a veces piensa demasiado y escribe mientras toma mate cuando el hecatombe mental aprieta. Bienvenido a donde nada está del todo claro, pero algo siempre sale.',
+};
+
 export default function HomePage() {
   return (
     <div className="space-y-6">
-      <Hero
-        description="Qué se yo… soy un pibe que piensa demasiado y escribe cuando el caos mental aprieta. Bienvenido a donde nada está del todo claro, pero algo siempre sale."
-        title="Bienvenido a mi espacio personal"
-      />
+      <Hero title={info.title} description={info.description} />
       <FeaturedPosts />
     </div>
   );
--- src/lib/services.ts
+++ src/lib/services.ts
@@ -2,10 +2,14 @@ const API_URL =
   process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_API_URL : 'http://localhost:3000';
 
 export async function getFeaturedPosts() {
-  const res = await fetch(`${API_URL}/api/posts?where[featured][equals]=true`);
+  const res = await fetch(`${API_URL}/api/posts?where[featured][equals]=true`, {
+    cache: 'no-store',
+  });
+  if (!res.ok) throw new Error('Error getting post by slug');
+
   const data = await res.json();
 
-  return data.docs;
+  return data.docs || [];
 }
 
 export async function getPosts(categorySlug?: string) {
@@ -15,15 +19,19 @@ export async function getPosts(categorySlug?: string) {
     url += `?where[category.slug][equals]=${categorySlug}`;
   }
 
-  const res = await fetch(url);
+  const res = await fetch(url, { cache: 'no-store' });
+  if (!res.ok) throw new Error('Error getting posts');
+
   const data = await res.json();
 
   return data.docs;
 }
-export async function getPostBySlug(slug: string) {
-  const res = await fetch(`${API_URL}/api/posts/?where[slug][equals]=${slug}`);
 
-  if (!res.ok) throw new Error('ERROR');
+export async function getPostBySlug(slug: string) {
+  const res = await fetch(`${API_URL}/api/posts/?where[slug][equals]=${slug}`, {
+    cache: 'no-store',
+  });
+  if (!res.ok) throw new Error('Error getting post by slug');
 
   const data = await res.json();
   return data.docs?.[0] ?? null;
@@ -31,6 +39,8 @@ export async function getPostBySlug(slug: string) {
 
 export async function getCategories() {
   const res = await fetch(`${API_URL}/api/categories`);
+  if (!res.ok) throw new Error('Error getting categories');
+
   const data = await res.json();
 
   return data.docs;
