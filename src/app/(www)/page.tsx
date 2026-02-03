f088eba40f5e966ce19ba094cfba3e5821499dfb
Nacho <166555602+figueroaignacio@users.noreply.github.com>
2026-02-03 03:42:36 -0300

    feat: revise title and description for HomePage
    Updated the title and description in the info object.

--- src/app/(www)/page.tsx
+++ src/app/(www)/page.tsx
@@ -1,13 +1,13 @@
-// Components
 import { PostListLoader } from '@/components/post-card-loader';
 import { Hero } from '@/sections/hero';
 import { RecentPosts } from '@/sections/recent-post';
 import { Suspense } from 'react';
 
 const info = {
-  title: 'Bienvenido a mi espacio personal',
-  description: 'Mi nombre es Nacho, me gusta escribir de las cosas que pasan por mi cabeza.',
+  title: 'Bienvenido a mi rincón de internet',
+  description: 'Soy Nacho. Acá escribo ideas, dudas y conclusiones que parecían buenas a las 2 a.m dentro de mi cabeza. Algunas incluso lo son. Y en otras probablemente cambie de opinión en un futuro'
 };
+
 
 export default async function HomePage() {
   return (
