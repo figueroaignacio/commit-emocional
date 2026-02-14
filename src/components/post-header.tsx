9008617a333be23c8cc29d7a29d149012c2efaec
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-02-14 10:04:17 -0300

    fix: idk what i did

--- src/components/post-header.tsx
+++ src/components/post-header.tsx
@@ -2,13 +2,16 @@ import { formatDate } from '@/lib/utils';
 import type { Post } from '@/payload-types';
 import { BackButton } from './back-button';
 
-interface PostHeaderProps extends Pick<Post, 'title' | 'description' | 'createdAt'> {}
-
-export function PostHeader({ description, title, createdAt }: PostHeaderProps) {
+export function PostHeader({
+  description,
+  title,
+  createdAt,
+}: Pick<Post, 'title' | 'description' | 'createdAt'>) {
   return (
     <div className="prose-container space-y-4 border-border border-b pb-4">
       <BackButton />
       <div className="space-y-4">
+        <p className="font-light text-sm">Escrito por Nacho el {formatDate(createdAt)}</p>
         <h1 className="text-3xl font-semibold tracking-tight text-foreground">{title}</h1>
         {description ? (
           <p className="text-muted-foreground font-light text-pretty leading-relaxed">
@@ -16,7 +19,6 @@ export function PostHeader({ description, title, createdAt }: PostHeaderProps) {
           </p>
         ) : null}
       </div>
-      <span className="font-light">Escrito por Nacho el {formatDate(createdAt)}</span>
     </div>
   );
 }
