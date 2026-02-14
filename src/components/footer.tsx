baf964a76a60999c1e93caf93455c77430cf806f
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-02-14 09:56:41 -0300

    fix: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,9 @@
 export function Footer() {
   return (
-    <footer className="mt-20 pt-8 border-t border-border py-12 container">
-      <p className="text-muted-foreground font-light text-center">© 2025 Commit Emocional</p>
+    <footer className="mt-20 pt-8 border-t border-border py-12">
+      <div className="container">
+        <p className="text-muted-foreground font-light text-center">© 2025 Commit Emocional</p>
+      </div>
     </footer>
   );
 }
